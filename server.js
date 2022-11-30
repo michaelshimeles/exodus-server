fc24ee1528314225a7b6c2b57a76c1a043629734
Michael Shimeles <michaelwasihun96@gmail.com>
2022-11-30 14:36:48 -0500

    Added a top collections endpoint for my topcollections endpoint

--- server.js
+++ server.js
@@ -12,184 +12,202 @@ app.use(express.json());
 
 // Sales Chart + Sales Card With Images
 app.get("/sales/:id", (req, res) => {
-    let config = {
-      headers: {
-        "accept-encoding": "*",
-      },
-    };
-  
-    axios
-      .get(
-        `https://api.reservoir.tools/sales/v4?includeTokenMetadata=true&collection=${req.params.id}&x-api-key=${RESEVOIR_API_KEY}`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-  
-        const salesData = response.data.sales.map((sale) => {
-          return {
-            name: sale.token.collection.name,
-            tokenId: sale.token.tokenId,
-            orderSource: sale.orderSource,
-            image: sale.token.image,
-            txHash: sale.txHash,
-            timestamp: sale.timestamp,
-            priceInEth: sale.price.amount.decimal,
-            priceInUsd: sale.price.amount.usd,
-            washTradingScore: sale.washTradingScore,
-          };
-        });
-  
-        console.log(salesData);
-        res.status(200).send({ data: salesData, original: response.data });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+    },
+  };
+
+  axios
+    .get(
+      `https://api.reservoir.tools/sales/v4?includeTokenMetadata=true&collection=${req.params.id}&x-api-key=${RESEVOIR_API_KEY}`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data);
+
+      const salesData = response.data.sales.map((sale) => {
+        return {
+          name: sale.token.collection.name,
+          tokenId: sale.token.tokenId,
+          orderSource: sale.orderSource,
+          image: sale.token.image,
+          txHash: sale.txHash,
+          timestamp: sale.timestamp,
+          priceInEth: sale.price.amount.decimal,
+          priceInUsd: sale.price.amount.usd,
+          washTradingScore: sale.washTradingScore,
+        };
       });
-  });
-  
+
+      console.log(salesData);
+      res.status(200).send({ data: salesData, original: response.data });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
+
 // Listing Distribution Chart & Listings Card With Images
 app.get("/listings/:id", (req, res) => {
-    let config = {
-      headers: {
-        "accept-encoding": "*",
-      },
-    };
-    axios
-      .get(
-        `https://api.reservoir.tools/orders/asks/v3?contracts=${req.params.id}&status=active&includePrivate=false&includeMetadata=true&x-api-key=${RESEVOIR_API_KEY}&sortBy=createdAt&limit=100`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.status(200).json({ data: response.data });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
-      });
-  });
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+    },
+  };
+  axios
+    .get(
+      `https://api.reservoir.tools/orders/asks/v3?contracts=${req.params.id}&status=active&includePrivate=false&includeMetadata=true&x-api-key=${RESEVOIR_API_KEY}&sortBy=createdAt&limit=100`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).json({ data: response.data });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
 
 app.get("/info/resevoir/:id", (req, res) => {
-    let config = {
-        headers: {
-          "accept-encoding": "*",
-        },
-      };
-      axios
-      .get(
-        `https://api.reservoir.tools/collections/v5?contract=${req.params.id}&includeTopBid=false&normalizeRoyalties=false&sortBy=allTimeVolume&limit=20&x-api-key=${RESEVOIR_API_KEY}`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.status(200).json({ data: response.data });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
-      });
-    // 
-})
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+    },
+  };
+  axios
+    .get(
+      `https://api.reservoir.tools/collections/v5?contract=${req.params.id}&includeTopBid=false&normalizeRoyalties=false&sortBy=allTimeVolume&limit=20&x-api-key=${RESEVOIR_API_KEY}`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).json({ data: response.data });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
+
+app.get("/topcollections", (req, res) => {
+  // https://api.reservoir.tools/collections/v5
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+      "x-api-key": "c068dde1-dc4a-57d1-9432-da9e4872db73",
+    },
+  };
+
+  axios
+    .get(`https://api.reservoir.tools/collections/v5`, config)
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
 
 // Collection Stats
 // tokenListedCount, holders, totalSupply, floorPrice
 app.get("/stats/:id", (req, res) => {
-    let config = {
-      headers: {
-        "accept-encoding": "*",
-        "X-API-KEY": MODULE_API_KEY,
-      },
-    };
-    axios
-      .get(
-        `https://api.modulenft.xyz/api/v2/eth/nft/stats?contractAddress=${req.params.id}`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.status(200).json(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
-      });
-  });
-
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+      "X-API-KEY": MODULE_API_KEY,
+    },
+  };
+  axios
+    .get(
+      `https://api.modulenft.xyz/api/v2/eth/nft/stats?contractAddress=${req.params.id}`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
 
 // Collection Info
 app.get("/info/:id", (req, res) => {
-    let config = {
-      headers: {
-        "accept-encoding": "*",
-        "X-API-KEY": MODULE_API_KEY,
-      },
-    };
-    axios
-      .get(
-        `https://api.modulenft.xyz/api/v2/eth/nft/collection?contractAddress=${req.params.id}`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data.dat);
-        res.status(200).json(response.data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
-      });
-  });
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+      "X-API-KEY": MODULE_API_KEY,
+    },
+  };
+  axios
+    .get(
+      `https://api.modulenft.xyz/api/v2/eth/nft/collection?contractAddress=${req.params.id}`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data.dat);
+      res.status(200).json(response.data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
 
 // Sales Chart + Sales Card With Images
 app.get("/sales/module/:id", (req, res) => {
-    let config = {
-      headers: {
-        Accept: "application/json",
-        "accept-encoding": "*",
-        "X-API-KEY": MODULE_API_KEY,
-      },
-    };
-    axios
-      .get(
-        `https://api.modulenft.xyz/api/v2/eth/nft/sales?count=100&offset=0&sortDirection=timeDesc&contractAddress=${req.params.id}&withMetadata=true"`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.status(200).json(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
-      });
-  });
-  
-// Holder distribution 
+  let config = {
+    headers: {
+      Accept: "application/json",
+      "accept-encoding": "*",
+      "X-API-KEY": MODULE_API_KEY,
+    },
+  };
+  axios
+    .get(
+      `https://api.modulenft.xyz/api/v2/eth/nft/sales?count=100&offset=0&sortDirection=timeDesc&contractAddress=${req.params.id}&withMetadata=true"`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
+
+// Holder distribution
 app.get("/owner/:id", (req, res) => {
-    let config = {
-      headers: {
-        Accept: "application/json",
-        "accept-encoding": "*",
-        "X-API-KEY": MODULE_API_KEY,
-      },
-    };
-    axios
-      .get(
-        `https://api.modulenft.xyz/api/v2/eth/nft/ownerDistribution?contractAddress=${req.params.id}`,
-        config
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.status(200).json(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(404).json(error);
-      });
-  });
+  let config = {
+    headers: {
+      Accept: "application/json",
+      "accept-encoding": "*",
+      "X-API-KEY": MODULE_API_KEY,
+    },
+  };
+  axios
+    .get(
+      `https://api.modulenft.xyz/api/v2/eth/nft/ownerDistribution?contractAddress=${req.params.id}`,
+      config
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
 
 app.listen(PORT, (_req, _res) => {
-    console.log("Server is live");
-  });
-  
+  console.log("Server is live");
+});
