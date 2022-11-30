66e6ea2aee4f7e015882a380753464de09acb968
Michael Shimeles <michaelwasihun96@gmail.com>
2022-11-30 13:38:58 -0500

    Holder distribution chart data

--- server.js
+++ server.js
@@ -144,7 +144,29 @@ app.get("/sales/module/:id", (req, res) => {
       });
   });
   
-  
+// Holder distribution 
+app.get("/owner/:id", (req, res) => {
+    let config = {
+      headers: {
+        Accept: "application/json",
+        "accept-encoding": "*",
+        "X-API-KEY": MODULE_API_KEY,
+      },
+    };
+    axios
+      .get(
+        `https://api.modulenft.xyz/api/v2/eth/nft/ownerDistribution?contractAddress=${req.params.id}`,
+        config
+      )
+      .then((response) => {
+        console.log(response.data);
+        res.status(200).json(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(404).json(error);
+      });
+  });
 
 app.listen(PORT, (_req, _res) => {
     console.log("Server is live");
