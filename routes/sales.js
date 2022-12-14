da72d4bde1f67ab1a2f0993c75a484136b0683f5
Michael Shimeles <michaelwasihun96@gmail.com>
2022-12-14 01:59:28 -0500

    Changed sales chart api to index based on different time frames

--- routes/sales.js
+++ routes/sales.js
@@ -41,6 +41,43 @@ router.get("/:id", (req, res) => {
     });
 });
 
+
+router.post("/:id", (req, res) => {
+  let config = {
+    headers: {
+      "accept-encoding": "*",
+      "x-api-key": RESEVOIR_API_KEY,
+    },
+  };
+
+  axios
+    .get(
+      `https://api.reservoir.tools/sales/v4?includeTokenMetadata=true&collection=${req.params.id}&startTimestamp=${req.body.start}&endTimestamp=${req.body.end}&limit=1000`,
+      config
+    )
+    .then((response) => {
+      const salesData = response.data.sales.map((sale) => {
+        return {
+          name: sale.token.collection.name,
+          tokenId: sale.token.tokenId,
+          orderSource: sale.orderSource,
+          image: sale.token.image,
+          txHash: sale.txHash,
+          timestamp: sale.timestamp,
+          priceInEth: sale.price.amount.decimal,
+          priceInUsd: sale.price.amount.usd,
+          washTradingScore: sale.washTradingScore,
+        };
+      });
+
+      // console.log(salesData);
+      res.status(200).send(salesData);
+    })
+    .catch((error) => {
+      res.status(404).json(error);
+    });
+});
+
 router.post("/time/:id", (req, res) => {
   let config = {
     headers: {
